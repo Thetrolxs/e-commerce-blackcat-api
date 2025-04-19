ad02d6152b463238edf6a2c5cbcccf88948bfff9
Alonso Rojas <alonso.rojas@alumnos.ucn.cl>
2025-04-19 16:07:25 -0400

    feat: create User DTOs based on IdentityUser model

---
+++ src/Dtos/userDto.cs
@@ -0,0 +1,9 @@
+namespace e_commerce_blackcat_api.Src.Dtos;
+
+public class UserDto
+{
+    public required string Id { get; set; }
+    public required string FullName { get; set; }
+    public required string Email { get; set; }
+    public required string PhoneNumber { get; set; }
+}
