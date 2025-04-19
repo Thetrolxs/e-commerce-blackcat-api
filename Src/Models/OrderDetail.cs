83c5cc06bf542443a8e7a9c3afd4e003c77a19cf
Thetrolxs <moralesharnisch@gmail.com>
2025-04-19 10:17:50 -0400

    fix: OrderId access in OrderDetail

--- Src/Models/OrderDetail.cs
+++ Src/Models/OrderDetail.cs
@@ -11,6 +11,8 @@ namespace e_commerce_blackcat_api.Src.Models
     public class OrderDetail
     {
         public int Id {get; set;}
+        
+        public int OrderId {get; set;} 
         public Order Order {get; set;} = null!;
 
         public int ProductId {get; set;}
