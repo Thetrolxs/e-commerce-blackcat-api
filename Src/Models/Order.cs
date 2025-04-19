52f5e2e9ad2c88d8baebc9b3e6d5e145c7ad3b9f
Thetrolxs <moralesharnisch@gmail.com>
2025-04-19 08:04:02 -0400

    feat: add Order and OrderDetail models

---
+++ Src/Models/Order.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_commerce_blackcat_api.Src.Models
+{
+    // Representa una orden de compra realizada por el usuario
+    public class Order
+    {
+        public int Id {get; set;}
+        public string UserId {get; set;} = string.Empty;
+
+        public DateTime CreatedOrder {get; set;} = DateTime.UtcNow;
+
+        public string Status {get; set;} = string.Empty;
+        public int TotalAmount {get; set;}
+
+        //Relación
+        public ICollection<OrderDetail> OrderDetails {get; set;} = null!;
+    }
+}
