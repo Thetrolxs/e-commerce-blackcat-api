3393e86131245c8be97dbf90a95c061d410a0b90
Thetrolxs <moralesharnisch@gmail.com>
2025-04-20 19:58:10 -0400

    feat: add relations in datacontext

--- Src/Data/DataContext.cs
+++ Src/Data/DataContext.cs
@@ -13,5 +13,38 @@ namespace e_commerce_blackcat_api.Data;
         public DbSet<Order> Orders => Set<Order>();
         public DbSet<OrderDetail> OrderDetails => Set<OrderDetail>();
 
-        
+        protected override void OnModelCreating(ModelBuilder builder)
+            {
+                base.OnModelCreating(builder);
+
+                //CartItem - User
+                builder.Entity<CartItem>()
+                    .HasOne(c => c.User)
+                    .WithMany(u => u.CartItems)
+                    .HasForeignKey(c => c.UserId);
+
+                //CartItem - Product
+                builder.Entity<CartItem>()
+                    .HasOne(c => c.Product)
+                    .WithMany(p => p.CartItems)
+                    .HasForeignKey(c => c.ProductId);
+
+                //Order - User
+                builder.Entity<Order>()
+                    .HasOne(o => o.User)
+                    .WithMany(u => u.Orders)
+                    .HasForeignKey(o => o.UserId);
+
+                //OrderDetail - Order
+                builder.Entity<OrderDetail>()
+                    .HasOne(od => od.Order)
+                    .WithMany(o => o.OrderDetails)
+                    .HasForeignKey(od => od.OrderId);
+
+                //OrderDetail - Product
+                builder.Entity<OrderDetail>()
+                    .HasOne(od => od.Product)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(od => od.ProductId);
+            }        
     }
