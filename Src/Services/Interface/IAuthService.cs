fa46e981d8a23a3326f356e57b454c622db309c1
Thetrolxs <moralesharnisch@gmail.com>
2025-04-20 20:33:52 -0400

    feat: add auth service

---
+++ Src/Services/Interface/IAuthService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_commerce_blackcat_api.Src.Services.Interface
+{
+    public interface IAuthService
+    {
+        
+    }
+}
