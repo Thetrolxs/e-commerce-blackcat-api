cde1068330317a9eb5a67db920f6a6f8a0d56664
Alonso Rojas <alonso.rojas@alumnos.ucn.cl>
2025-05-08 23:33:00 -0400

    feat: implement ProductMapper for converting between entities and DTOs

---
+++ Src/Mappers/ProductMapper.cs
@@ -0,0 +1,53 @@
+using e_commerce_blackcat_api.Models;
+using e_commerce_blackcat_api.Src.Dtos;
+
+
+namespace e_commerce_blackcat_api.Src.Mappers;
+
+public static class ProductMapper
+{
+    public static ProductDto ToProductDto(this Product product)
+    {
+        return new ProductDto
+        {
+            Id = product.Id,
+            Title = product.Title,
+            Description = product.Description,
+            Price = product.Price,
+            Stock = product.Stock,
+            Category = product.Category,
+            Brand = product.Brand,
+            IsNew = product.IsNew,
+            CreatedAt = product.CreatedAt
+        };
+    }
+
+    public static Product ToProductFromCreateDto(this ProductCreateDto dto)
+    {
+        return new Product
+        {
+            Title = dto.Title,
+            Description = dto.Description,
+            Price = dto.Price,
+            Stock = dto.Stock,
+            Category = dto.Category,
+            Brand = dto.Brand,
+            IsNew = dto.IsNew,
+            CreatedAt = DateTime.UtcNow
+        };
+    }
+
+    public static void UpdateProductFromDto(this Product product, ProductUpdateDto dto)
+    {
+        product.Title = dto.Title;
+        product.Description = dto.Description;
+        product.Price = dto.Price;
+        product.Stock = dto.Stock;
+        product.Category = dto.Category;
+        product.Brand = dto.Brand;
+        product.IsNew = dto.IsNew;
+        
+    }
+
+    
+}
