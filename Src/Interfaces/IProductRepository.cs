e02b3a90837bacaa0c5cd57d396f65182535173a
Alonso Rojas <alonso.rojas@alumnos.ucn.cl>
2025-05-10 02:03:06 -0400

    feat: declare GetPagedProductsAsync method in IProductRepository for filtered queries

--- Src/Interfaces/IProductRepository.cs
+++ Src/Interfaces/IProductRepository.cs
@@ -1,8 +1,11 @@
 using e_commerce_blackcat_api.Models;
+using e_commerce_blackcat_api.Helpers;
+
 namespace e_commerce_blackcat_api.Interfaces;
 public interface IProductRepository
 {
     Task<IEnumerable<Product>> GetAllAsync();
+    Task<PagedResult<Product>> GetPagedProductsAsync(ProductParams queryParams);
     Task<Product?> GetByIdAsync(int id);
     Task AddAsync(Product product);
     Task Update(Product product);
