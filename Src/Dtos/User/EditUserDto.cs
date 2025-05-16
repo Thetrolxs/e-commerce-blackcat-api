0d2713718ef3db4164bb54fd1ebeb06e378def84
Thetrolxs <moralesharnisch@gmail.com>
2025-05-16 17:17:50 -0400

    fix: fullname attribute

--- Src/Dtos/User/EditUserDto.cs
+++ Src/Dtos/User/EditUserDto.cs
@@ -7,14 +7,9 @@ namespace e_commerce_blackcat_api.Src.Dtos
     public class EditUserDto
     {
         [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]+$", ErrorMessage = "El Nombre solo puede contener caracteres del abecedario español.")]
-        [MinLength(2, ErrorMessage = "El nombre debe tener al menos 2 caracteres.")]
+        [MinLength(8, ErrorMessage = "El nombre debe tener al menos 8 caracteres.")]
         [MaxLength(255, ErrorMessage = "El nombre debe tener a lo más 255 caracteres.")]
-        public string? FirstName { get; set; }
-
-        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]+$", ErrorMessage = "El apellido solo puede contener caracteres del abecedario español.")]
-        [MinLength(2, ErrorMessage = "El apellido debe tener al menos 2 caracteres.")]
-        [MaxLength(255, ErrorMessage = "El apellido debe tener a lo más 255 caracteres.")]
-        public string? LastName { get; set; }
+        public string? FullName { get; set; }
 
         [RegularExpression(@"^\d{9}$", ErrorMessage = "El número de teléfono debe tener exactamente 9 dígitos.")]
         public string? PhoneNumber { get; set; }
