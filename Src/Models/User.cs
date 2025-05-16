a3c2f222db37b546408cb96d0d38a313f510dd82
Thetrolxs <moralesharnisch@gmail.com>
2025-05-15 23:20:21 -0400

    feat: add atributes in user

--- Src/Models/User.cs
+++ Src/Models/User.cs
@@ -7,12 +7,21 @@ namespace e_commerce_blackcat_api.Src.Models
     // Usuario identity con nombre completo
     public class User : IdentityUser
     {
-        public string FullName {get; set;} = string.Empty;
-
-        public ShippingAddres? ShippingAddres { get; set; }
-
+        public required string FirstName {get; set;} 
+        public required string LastName {get; set;}
+        // Fecha en la que se registro el usuario
+        public DateTime UserRegister {get; set;}
+        // Ultimo acceso a la cuenta del usuario
+        public DateTime? LastAccess {get; set;}
+        public DateTime Birthday {get; set;}
+        // Estado del usuario (True = Activo, False = Desactivado)
+        public bool IsActive {get; set;}
+        // Posible razon que el usuario fue desactivado.
+        public string? DesactivationReason {get; set;}
         //Relaciones
         public ICollection<Order> Orders {get; set;} = null!;
         public ICollection<CartItem> CartItems {get; set;} = null!;
+        // Datos del domicilio del usuario
+        public ShippingAddres? ShippingAddres { get; set; }
     }
 }
