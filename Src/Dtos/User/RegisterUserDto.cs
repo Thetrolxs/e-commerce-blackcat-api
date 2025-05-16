14e245bf94ba12f4acc03947ae0b72c8681c477d
Thetrolxs <moralesharnisch@gmail.com>
2025-05-16 17:17:25 -0400

    feat: add RegisterUserDto

---
+++ Src/Dtos/User/RegisterUserDto.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+using e_commerce_blackcat_api.Src.Helpers;
+
+namespace e_commerce_blackcat_api.Src.Dtos.User
+{
+    public class RegisterUserDto
+    {
+        [Required(ErrorMessage = "El Nombre es obligatorio.")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]+$", ErrorMessage = "El Nombre solo puede contener caracteres del abecedario español.")]
+        [MinLength(8, ErrorMessage = "El Nombre debe tener al menos 8 caracteres.")]
+        [MaxLength(255, ErrorMessage = "El Nombre debe tener a lo más 255 caracteres.")]
+        public string FullName { get; set; } = string.Empty;
+
+             
+        [Required(ErrorMessage = "La Fecha de Nacimiento es obligatoria.")]
+        [DataType(DataType.Date)]  
+        [DateValidation]
+        public string Birthday { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El Email no tiene un formato válido.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El número de teléfono es obligatorio.")]
+        [RegularExpression(@"^\d{9}$", ErrorMessage = "El número de teléfono debe tener exactamente 9 dígitos.")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La Contraseña es obligatoria.")]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]+$", ErrorMessage = "La Contraseña debe ser alfanumérica.")]
+        [MinLength(8, ErrorMessage = "La Contraseña debe tener al menos 8 caracteres.")]
+        [MaxLength(20, ErrorMessage = "La Contraseña debe tener a lo más 20 caracteres.")]
+        public string Password { get; set; } = string.Empty;
+
+        [Compare("Password", ErrorMessage = "Las Contraseñas no coinciden.")]
+        public string ConfirmPassword { get; set; } = string.Empty;        
+    }
+}
